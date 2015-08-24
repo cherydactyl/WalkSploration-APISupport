d1373d294e433a0a91d05d54d05d6ff93ce6d1c8
aKratzke <arosekratzke@gmail.com>
2015-08-24 17:05:14 -0400

    Added note in Point of Interest

--- WalkSploration/Models/PointOfInterest.cs
+++ WalkSploration/Models/PointOfInterest.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Device.Location;
+using System.Device.Location;    /* more info here:  https://msdn.microsoft.com/en-us/library/system.device.location.geocoordinate(v=vs.110).aspx
+                                 Install package on command line: Install-Package System.Device.Location.Portable   -  ak 8.24.15*/
 
 namespace WalkSploration.Models
 {
     public class PointOfInterest
     {
-        int _ID;
-        GeoCoordinate location;
-        String name;                //a short label
-        String address;             //street address
-        String category;            //for future developments
+        int _ID;                    // Point of Interest ID
+        GeoCoordinate location;     // 
+        String name;                // A short label
+        String address;             // Street address
+        String category;            // For future developments
     }
 }
