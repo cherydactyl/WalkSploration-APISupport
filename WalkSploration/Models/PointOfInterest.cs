e23ab16fd6d9c1a11d1b965c8ccc7f09016dad67
cherydactyl <cheryl.orosz@gmail.com>
2015-08-24 16:23:21 -0400

    Initial Commit

---
+++ WalkSploration/Models/PointOfInterest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Device.Location;
+
+namespace WalkSploration.Models
+{
+    public class PointOfInterest
+    {
+        int _ID;
+        Location location;
+        String address;             //street address
+        
+    }
+}
