7b6e309b91e5d9a53788f55832a52d75bd4b7606
aKratzke <arosekratzke@gmail.com>
2015-08-25 10:21:36 -0400

    Update per 8.24.15

--- WalkSploration/Models/PointOfInterest.cs
+++ WalkSploration/Models/PointOfInterest.cs
@@ -2,14 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Device.Location;    /* more info here:  https://msdn.microsoft.com/en-us/library/system.device.location.geocoordinate(v=vs.110).aspx
-                                 Install package on command line: Install-Package System.Device.Location.Portable   -  ak 8.24.15*/
+using System.Device.Location;   /* more info here:  https://msdn.microsoft.com/en-us/library/system.device.location.geocoordinate(v=vs.110).aspx
+                                You may not be able to run this properly without adding the following NuGet Package in the 
+                                Package Manager Console command line: 'Install-Package System.Device.Location.Portable'   -  ak 8.24.15*/
 
 namespace WalkSploration.Models
 {
     public class PointOfInterest
     {
-        int _ID;                    // Point of Interest ID
+        int place_ID;               // Point of Interest ID
         GeoCoordinate location;     // 
         String name;                // A short label
         String address;             // Street address
