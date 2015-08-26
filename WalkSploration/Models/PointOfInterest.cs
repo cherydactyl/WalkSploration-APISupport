640170ab0b95c7378d822162e1aa7d8ac70ad42e
cherydactyl <cheryl.orosz@gmail.com>
2015-08-26 14:20:32 -0400

    Removed unneeded using statement from PoI model

--- WalkSploration/Models/PointOfInterest.cs
+++ WalkSploration/Models/PointOfInterest.cs
@@ -2,9 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Device.Location;   /* more info here:  https://msdn.microsoft.com/en-us/library/system.device.location.geocoordinate(v=vs.110).aspx
-                                You may not be able to run this properly without adding the following NuGet Package in the 
-                                Package Manager Console command line: 'Install-Package System.Device.Location.Portable'   -  ak 8.24.15*/
+//using System.Device.Location;   /* more info here:  https://msdn.microsoft.com/en-us/library/system.device.location.geocoordinate(v=vs.110).aspx
+//                                You may not be able to run this properly without adding the following NuGet Package in the 
+//                                Package Manager Console command line: 'Install-Package System.Device.Location.Portable'   -  ak 8.24.15*/
 
 namespace WalkSploration.Models
 {
