e465663d6cc6f3e571c86b23a494f629553528ae
cherydactyl <cheryl.orosz@gmail.com>
2015-08-27 16:12:07 -0400

    WebRequest helper function added
    processHttpJSonReq(string URI) in Home controller

--- WalkSploration/Controllers/HomeController.cs
+++ WalkSploration/Controllers/HomeController.cs
@@ -54,22 +54,32 @@ namespace WalkSploration.Controllers
             URI += "types=park";
 
             //request processing
+
             WebRequest request = WebRequest.Create(URI);
             request.Method = "GET";
             request.ContentType = "application/json";
             WebResponse response = request.GetResponse();
 
-            Stream dataStream = response.GetResponseStream();
+            Stream dataStream = processHttpJSonReq(URI).GetResponseStream();       // ???
 
 
             //create a new list
-            List<PointOfInterest> PoIs = new List<PointOfInterest>();
+            List<PointOfInterest> PoIs = new List<PointOfInterest>();       //start with empty list
 
             //iterate over the json and parse into PointsOfInterest, placing each in list
             
 
             //return processed list
             return PoIs;
+        }
+
+        WebResponse processHttpJSonReq(string URI)
+        {
+            WebRequest request = WebRequest.Create(URI);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            WebResponse response = request.GetResponse();
+            return response;
         }
 
     }
