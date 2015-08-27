0b24e82a8f5539d39107809c7460ece08241b55a
aKratzke <arosekratzke@gmail.com>
2015-08-27 10:36:25 -0400

    Got the input time to proces through the controller and return 3 set error message

--- WalkSploration/Controllers/HomeController.cs
+++ WalkSploration/Controllers/HomeController.cs
@@ -13,35 +13,63 @@ namespace WalkSploration.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
-        {
-            return View();
-        }
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
 
-        public ActionResult SearchIndex() {
-            return View();
-        }
-        
-        //Get Point of Interset
-        public ActionResult SearchIndex(float? latitude, float? longitude)
-        {
-            ViewBag.InputLatitude = latitude;
-            ViewBag.InputLongitude = longitude;
 
-            if (latitude == null || longitude == null)
+        public ActionResult Index(string time)
+        {
+            var inputTime = Convert.ToInt32(time);
+            var timeHalf = ((inputTime + .20f) / 2) * 1.1f;
+
+            //create and initialize two floating point variables
+            //float timeWhole = 0;
+            //float timeHalf = 0;
+            //cast time to float
+            //bool checkInput = Int32.TryParse(time, out timeWhole);
+            //timeHalf = ((timeWhole + .20f) / 2) * 1.1f;
+
+            if (time == null)
             {
-                ModelState.AddModelError("", "Unable to process request. Please ensure that the correct latitude and longitude have been entered.");
+                ModelState.AddModelError("", "Unable to process request. Please ensure that a valid time has been entered.");
+            }
+            else if (inputTime > 60)
+            {
+                ModelState.AddModelError("", "Please enter a time equal to or less than 60 minutes.");
+            }
+            else if (inputTime <= 0)
+            {
+                ModelState.AddModelError("", "Please enter a time equal to or greater than 1.");
             }
 
-            //PointOfInterest poi = db.PointsofInterest.Find(id);
-            //if (poi == null)
-            //{
-            //    return HttpNotFound();
-            //}
-            
-            
-            return View();
+
+            return View(timeHalf);
         }
+
+        //Commented out on 8.27.15 to incorporate code with Vaneitta
+
+        ////Get Point of Interset
+        //public ActionResult SearchIndex(float? latitude, float? longitude)
+        //{
+        //    ViewBag.InputLatitude = latitude;
+        //    ViewBag.InputLongitude = longitude;
+
+        //    if (latitude == null || longitude == null)
+        //    {
+        //        ModelState.AddModelError("", "Unable to process request. Please ensure that the correct latitude and longitude have been entered.");
+        //    }
+
+        //    //PointOfInterest poi = db.PointsofInterest.Find(id);
+        //    //if (poi == null)
+        //    //{
+        //    //    return HttpNotFound();
+        //    //}
+
+
+        //    return View();
+        //}
 
         public ActionResult About()
         {
