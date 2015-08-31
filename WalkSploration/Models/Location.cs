db259fb469db1aad5567f59620ac1b9dce694fe5
aKratzke <arosekratzke@gmail.com>
2015-08-31 16:55:08 -0400

    Added Location Model

---
+++ WalkSploration/Models/Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WalkSploration.Models
+{
+    public class Location
+    {
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+
+    //public Location location() { }
+}
