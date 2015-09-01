3bfcefa50a81b0151219a544d020fd290dfe9b35
cherydactyl <cheryl.orosz@gmail.com>
2015-08-31 21:49:01 -0400

    Added constructors & other methods to PoI and Location

--- WalkSploration/Models/Location.cs
+++ WalkSploration/Models/Location.cs
@@ -7,8 +7,8 @@ namespace WalkSploration.Models
 {
     public class Location
     {
-        public decimal Latitude { get; set; }
-        public decimal Longitude { get; set; }
+        public decimal Latitude { get; }
+        public decimal Longitude { get; }
 
         public Location (decimal lat, decimal lon)
         {
--- WalkSploration/Models/PointOfInterest.cs
+++ WalkSploration/Models/PointOfInterest.cs
@@ -11,8 +11,9 @@ namespace WalkSploration.Models
     public class PointOfInterest
     {
         public int Id { get; set; }                    // Point of Interest ID
-        public decimal Latitude { get; set; }            // lattitude of location
-        public decimal Longitude { get; set; }           // longitude of location
+        public Location location { get; set; }               //prefer to make location not malleable
+        //public decimal Latitude { get; set; }            // lattitude of location
+        //public decimal Longitude { get; set; }           // longitude of location
         public string Name { get; set; }               // A short label
         public string Address { get; set; }            // Street address
         public string Category { get; set; }           // For future developments
@@ -25,5 +26,23 @@ namespace WalkSploration.Models
         //Navigation Property                    
         public User User { get; set; }           // For possibly associating with a user
 
+        public PointOfInterest(decimal lat, decimal lng, string googID)
+        {
+            this.location = new Location(lat, lng);
+            this.GooglePlaceId = googID;
+        }
+
+        public decimal getLatitude()
+        {
+            return this.location.Latitude;
+        }
+
+        public decimal getLongitude()
+        {
+            return this.location.Longitude;
+        }
+
     }
+
+
 }
