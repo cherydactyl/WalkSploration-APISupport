7c8383a8a330d7a6fe1acd27d7841d475f19c408
cherydactyl <cheryl.orosz@gmail.com>
2015-08-31 21:51:48 -0400

    Updated getPlaces and stubbed in screenPlaces in Home Controller
    Changed getPlaces to use the time in minutes and estimate 1 m/s waling
    speed to calculate radius for initial search; should speed processing
    for smaller time limits.  Also started building screenPlaces to use
    distance matrix API and screen candidates for whether they are in the
    Goldilocks band

--- WalkSploration/Controllers/HomeController.cs
+++ WalkSploration/Controllers/HomeController.cs
@@ -21,17 +21,17 @@ namespace WalkSploration.Controllers
     {
         public ActionResult Index()
         {
-            //Grand Circus 42.3347, -83.0497
-
+            //Grand Circus 42.3347, -83.0497; this is just a placeholder until actual start location set
             Location start = new Location((decimal)42.3347, (decimal)-83.0497);
+            //int time = 15;  //sample time
 
-            //screenPlaces(getPlaces((decimal)42.3347, (decimal)-83.0497));          
+            //screenPlaces(getPlaces((decimal)42.3347, (decimal)-83.0497), start, time);          
             return View();
 
         }
 
         //helper functions here
-        public List<PointOfInterest> getPlaces(decimal latitude, decimal longitude)
+        public List<PointOfInterest> getPlaces(decimal latitude, decimal longitude, int timeInMinutes)
         {
             //create query
             //build ("https://maps.googleapis.com/maps/api/place/nearbysearch/output?" + parameters)
@@ -42,8 +42,8 @@ namespace WalkSploration.Controllers
             URI += "key=" + (new Secrets()).GoogleAPIServerKey + "&";
             //location
             URI += "location=" + latitude.ToString() + "," + longitude.ToString() + "&";
-            //radius; 2 miles ~= 3200 meters; google requires radius in meters, max 50,000
-            URI += "radius=3200&";
+            //radius; estimate 1 meter per second walking speed
+            URI += "radius="+ (timeInMinutes * 60 /2).ToString() +"&";
             //types; start with "park" and possibly add more later
             //see https://developers.google.com/places/supported_types for list of types
             URI += "types=park";
@@ -62,29 +62,60 @@ namespace WalkSploration.Controllers
                 //iterate over the json and parse into PointsOfInterest, placing each in the list
                 foreach (var item in resultsArray)
                 {
-                    PointOfInterest point = new PointOfInterest();
-                    //extract and set the google place id in point
-                    point.GooglePlaceId = item.Children<JProperty>().FirstOrDefault(x => x.Name == "place_id").Value.ToString();
-                    //extract the lattitude from JSon structure and set in point
-                    point.Latitude = decimal.Parse(
+                    
+                    //extract the google place id
+                    string GooglePlaceId = item.Children<JProperty>().FirstOrDefault(x => x.Name == "place_id").Value.ToString();
+                    //extract the lattitude from JSon structuret
+                    decimal lat = decimal.Parse(
                         item.Children<JProperty>().FirstOrDefault(x => x.Name == "geometry").Value.
                         Children<JProperty>().FirstOrDefault(x => x.Name == "location").Value.
                         Children<JProperty>().FirstOrDefault(x => x.Name == "lat").Value.ToString());
-                    //extract the longitude from JSon structure and set in point
-                    point.Longitude = decimal.Parse(
+                    //extract the longitude from JSon structure
+                    decimal lng = decimal.Parse(
                         item.Children<JProperty>().FirstOrDefault(x => x.Name == "geometry").Value.
                         Children<JProperty>().FirstOrDefault(x => x.Name == "location").Value.
                         Children<JProperty>().FirstOrDefault(x => x.Name == "lng").Value.ToString());
+                    //create new PoI with above properties
+                    PointOfInterest point = new PointOfInterest(lat, lng, GooglePlaceId);
                     
+                    //add it to the list of candidates
                     candidatePoIs.Add(point);
-
-                    Debug.WriteLine(point.GooglePlaceId.ToString());
                 }
             }
-
             //return processed list
             return candidatePoIs;
         }
+
+        List<PointOfInterest> screenPlaces(List<PointOfInterest> candidates, Location start, int timeInMinutes)
+        {
+            //if the candidates list is empty, return the/an empty list
+            if (!(candidates.Any<PointOfInterest>()))
+                {
+                return candidates;
+            }
+
+            //calculate Goldilocks range (not too far but also not too close) in seconds
+            //use 90-100% of available one-way time to start; may need to iterate 
+            //note that these 
+            int ceiling = (timeInMinutes * 60)/2;   //max length of each leg of round trip
+            int floor = (ceiling * 10) / 9;         //min desired length of each leg of round trip
+
+            //form API call for distance matrix
+
+            //call the API and get JSon string back
+
+            //extract travel times
+
+            //create a new empty list
+            List<PointOfInterest> viable = new List<PointOfInterest>();
+
+            //step through 
+
+
+            return viable;
+        }
+
+
 
         string callAPIgetJSon (string URI)
         {
