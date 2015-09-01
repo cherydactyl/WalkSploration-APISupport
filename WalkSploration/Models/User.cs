607daa330cf9687ff83914817ef1b1a61ae97022
matthewmorris41 <matthewmorris41@gmail.com>
2015-09-01 16:08:46 -0400

    many to many
    did a many to many relationship between the users and the POI's.

--- WalkSploration/Models/User.cs
+++ WalkSploration/Models/User.cs
@@ -8,16 +8,24 @@ namespace WalkSploration.Models
 {
     public class User
     {
-        public int Id { get; set; }
+        public int UserID { get; set; }
         [Required]
         public string Name { get; set; }
 
         //Optional
         public string Email { get; set; }
+<<<<<<< HEAD
         public Location StartLocation { get; set; }
 
         //Check  whether or not these need to be virtual
         public virtual List<PointOfInterest> RecentTrips { get; set; }
         public virtual List<PointOfInterest> FavoritesList { get; set; }
+=======
+        public PointOfInterest StartLocation { get; set; }
+        public List<PointOfInterest> RecentTrips { get; set; }
+        public List<PointOfInterest> FavoritesList { get; set; }
+
+        public virtual ICollection<PointOfInterest> PointOfOntersts { get; set; }
+>>>>>>> many to many
     }
 }
