44550a9b9260f4a814a27dd23845baa89b2e235e
aKratzke <arosekratzke@gmail.com>
2015-09-05 15:55:46 -0400

    With added explanatory comments

--- WalkSploration/Scripts/LatLonDisplay.js
+++ WalkSploration/Scripts/LatLonDisplay.js
@@ -1,27 +1,27 @@
 ﻿
-function geoFindMe() {
-    var outputLat = document.getElementById("outLat");
-    var outputLon = document.getElementById("outLon");
+//function geoFindMe() {
+//    var outputLon = document.getElementById("outLon");  // Set to the value of the object with the id "outLon"
+//    var outputLat = document.getElementById("outLat");  // Set to the value of the object with the id "outLat"
 
-    if (!navigator.geolocation) {
-        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-        return;
-    }
+//    if (!navigator.geolocation) {   // If geolocation isn't available, then print the following
+//        output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
+//        return;
+//    }
 
-    function success(position) {
-        var latitude = position.coords.latitude;
-        var longitude = position.coords.longitude;
+//    function success(position) {  // takes in var position that was defined in Views > Shared > _Layout.cshtml
+//        var latitude = position.coords.latitude;
+//        var longitude = position.coords.longitude;
 
-        outputLat.innerHTML = "<p>Latitude is " + latitude + "°</p>";
-        outputLon.innerHTML = "<p>Longitude is " + longitude + "°</p>";
-    };
+//        outputLat.innerHTML = "<p>Latitude is " + latitude + "°</p>";
+//        outputLon.innerHTML = "<p>Longitude is " + longitude + "°</p>";
+//    };
 
-    function error() {
-        outputLat.innerHTML = "Unable to retrieve your location";
-        outputLon.innerHTML = " ";
-    };
+//    function error() {
+//        outputLat.innerHTML = "Unable to retrieve your location";
+//        outputLon.innerHTML = " ";
+//    };
 
-    outputLat.innerHTML = "<p>Locating…</p>";
+//    outputLat.innerHTML = "<p>Locating…</p>";
 
-    navigator.geolocation.getCurrentPosition(success, error);
-}
+//    navigator.geolocation.getCurrentPosition(success, error);
+//}
--- WalkSploration/Scripts/TimeSlider.js
+++ WalkSploration/Scripts/TimeSlider.js
@@ -1,3 +1,4 @@
-﻿function showValue(time) {
+﻿// Shows the current value of the range in View > Home > Index as a string
+function showValue(time) {
     document.getElementById("time-display").innerHTML = time;
 }
