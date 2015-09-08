25cf6b0da0489a35af748dbb55922526c154e60c
cherydactyl <cheryl.orosz@gmail.com>
2015-09-08 13:48:20 -0400

    Corrected ScreenPlaces method
    Previously screen places was adding PoIs that met goldilocks criteria to
    viable list more than once; refactored to correct.
    Also changed goldilocks zone floor to be 5 minutes less than ceiling
    (which is half the available time)

--- WalkSploration/Controllers/HomeController.cs
+++ WalkSploration/Controllers/HomeController.cs
@@ -25,17 +25,15 @@ namespace WalkSploration.Controllers
         }
 
         [HttpPost]
-        //public ActionResult Index(string timeInput, string startLatitude, string startLongitude)
-
         public ActionResult Index(FormCollection collection)
         {
-            Debug.WriteLine("Here!  ");
-            string testLat = collection["startLatitude"];
-            Debug.WriteLine(testLat);
             Location start = new Location(decimal.Parse(collection["startLatitude"]), decimal.Parse(collection["startLongitude"]));
             int time = int.Parse(collection["timeInput"]);
 
-            List<PointOfInterest> goldilocks = screenPlaces(getPlaces(start, time), start, time);
+            List<PointOfInterest> radarList = getPlaces(start, time);
+            Debug.WriteLine(radarList.Count);
+            List <PointOfInterest> goldilocks = screenPlaces(radarList, start, time);
+            Debug.WriteLine(goldilocks.Count);
             return View();
         }
 
@@ -112,6 +110,7 @@ namespace WalkSploration.Controllers
             URI += "destinations=";
             //iterate over candidate destinations, and add each location, separated by a pipe |
             int count = candidates.Count();
+            Debug.WriteLine(count);
             for (int i = 0; i < count; i++)
             {
                 URI += candidates[i].getLatitude().ToString() + "," + candidates[i].getLongitude().ToString();
@@ -127,7 +126,7 @@ namespace WalkSploration.Controllers
             //may need to iterate or otherwise process & if so, should probably save the times instead of make decisions in loop
             //note that these use integer math
             int ceiling = (timeInMinutes * 60) / 2;   //max length of each leg of round trip
-            int floor = (ceiling * 9) / 10;           //min length of each leg of round trip
+            int floor = ceiling - 300;          //min length of each leg of round trip; hardcode 5 minutes less one way
 
             var client = new WebClient();
             var values = System.Web.HttpUtility.ParseQueryString(string.Empty);
@@ -139,33 +138,25 @@ namespace WalkSploration.Controllers
             //remember there is only one destination, so the elements list the times to destinations in order
             if (string.Equals("OK", distanceResponse.Status, StringComparison.OrdinalIgnoreCase))
             {
-                foreach (var row in distanceResponse.Rows)
+                var elements = (distanceResponse.Rows[0]).Elements;
                 {
-                    foreach (var elements in row.Elements)
+                    for (int i = 0; i < count; i++)
                     {
-                        
-                        for (int i = 0; i < count; i++)
+                        //extract the time in seconds from origin to the current (i'th) destination
+                        if (string.Equals("OK", distanceResponse.Status, StringComparison.OrdinalIgnoreCase))
                         {
-
-                            var durationTest = elements.Duration.Value;
-
-
-                            //extract the time in seconds from origin to the current (i'th) destination
-                            if (string.Equals("OK", distanceResponse.Status, StringComparison.OrdinalIgnoreCase))
+                            // making a new int value to be able to better understand what the actual value is.
+                            int value = elements[i].Duration.Value;
+                            //compare to Goldilocks zone to evaluate and add to list if in the range
+                            if (value > floor && value <= ceiling)
                             {
-                                // making a new int value to be able to better understand what the actual value is.
-                                int value = elements.Duration.Value;
-                                //compare to Goldilocks zone to evaluate and add to list if in the range
-                                if (value > floor && value <= ceiling)
-                                {
-                                    viable.Add(candidates[i]);
-                                }
+                                viable.Add(candidates[i]);
                             }
                         }
                     }
                 }
             }
-            Console.WriteLine(candidates);
+            //Console.WriteLine(candidates);
             return viable;
         }
         string callAPIgetJSon(string URI)
